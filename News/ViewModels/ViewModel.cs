0d7225033af7cee48fd51c78b48fa4ff06127eb9
milkyborbon <mgborbon@urios.edu.ph>
2022-03-03 22:34:34 +0800

    Add project files.

---
+++ News/ViewModels/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace News.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
